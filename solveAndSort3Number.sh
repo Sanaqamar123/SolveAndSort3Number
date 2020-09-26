d3782bfc092aee364250e1d16d88e6a73d526073
Sanaqamar123 <sana.qamr@gmail.com>
2020-09-26 11:53:36 +0530

    Division of num1 by num2 is added to num3

--- solveAndSort3Number.sh
+++ solveAndSort3Number.sh
@@ -7,3 +7,6 @@ echo "First value of arithmetic calculation :" $result1
 
 result2==$(($(($num1*$num2))+$num3));
 echo "Second value of arithmetic calculation:" $result2
+
+result3=$(($num3+$(($num1/$num2))));
+echo "Third value of arithmetic calculation :" $result3
