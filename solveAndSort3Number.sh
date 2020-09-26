a96230d25a508fe893ff3c9104246f360d97f9a3
Sanaqamar123 <sana.qamr@gmail.com>
2020-09-26 11:37:41 +0530

    Multiple of num2 and num3 is added to num1

--- solveAndSort3Number.sh
+++ solveAndSort3Number.sh
@@ -1,3 +1,6 @@
 #!/bin/bash -x
 
 read -p "Enter three numbers for performing arithmetic calculation : " num1 num2 num3
+
+result1=$(($num1+$(($num2*$num3))));
+echo "First value of arithmetic calculation :" $result1
